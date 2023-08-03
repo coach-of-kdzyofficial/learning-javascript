9ae4297cb6668901dc26a4e7790cb090d3a18aaf
Muhammad Hanif Fadhlurrahman <niepep72@gmail.com>
2023-08-03 16:04:06 +0700

    sesi 1

---
+++ app.js
@@ -0,0 +1,40 @@
+// // // console.log('Sebelum Pembanding');
+// // // if (1 + 1 === 2) {
+// // //     console.log('Perhitungan masih betul');
+// // // }
+// // // console.log('Setelah Pembanding');
+
+// // let angka = Math.random()
+// // console.log(angka);
+
+// // if (angka >= 0.5) {
+// //     console.log('Angka lebih besar dari 0.5');
+// // }
+
+// // if (angka <= 0.5) {
+// //     console.log('Angka lebih kecil dari 0.5');
+// // }
+
+const iniHari = prompt('Input nama hari!').toLowerCase();
+
+if (iniHari === 'senin'){
+    console.log('Selamat bekerja');
+} else if (iniHari === 'sabtu'){
+    console.log('Semoga Jangan Cepat Berlalu');
+} else {
+    console.log('Yahh biasa aja');
+}
+
+// const nilai = '30'
+
+// if (nilai < 50){
+//     console.log('E')
+// }else if (nilai < 60){
+//     console.log('D')
+// }else if (nilai < 70){
+//     console.log('C')
+// }else if (nilai < 80){
+//     console.log('B')
+// }else if (nilai >=80){
+//     console.log('A')
+// }
